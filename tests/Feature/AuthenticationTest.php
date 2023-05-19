b9053276b2011c7c9db94ce06a280a2873951584
osama-mustafa <semsem6661@gmail.com>
2023-05-19 14:29:08 +0300

    Add test 3

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -8,6 +8,9 @@ use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
+
+    use RefreshDatabase;
+    
     public function test_login_page_is_loaded_successfully()
     {
         // Arrange
@@ -17,6 +20,43 @@ class AuthenticationTest extends TestCase
         $response = $this->get(route($loginRoute));
         
         // Assert
-        $response->assertSee('Login');
+        $response->assertOk();
+        $response->assertSeeText('Login');
+    }
+
+    public function test_register_page_is_loaded_successfully()
+    {
+        // Arrange
+        $registerRoute = 'register';
+
+        // Act
+        $response = $this->get(route($registerRoute));
+
+        // Arrange
+        $response->assertOk();
+        $response->assertSeeText('Register');
+    }
+
+    public function test_guest_user_can_register_an_account()
+    {
+        // Arrange
+        $data = [
+            'name' => 'first_user',
+            'email' => 'test_email@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ];
+        $registerRoute = 'register';
+
+        // Act
+        $response = $this->post(route($registerRoute), $data);
+
+        // Assert
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('home');
+        $this->assertAuthenticated();
+        $this->assertDatabaseHas('users', [
+            'email' => 'test_email@gmail.com'
+        ]);
     }
 }
